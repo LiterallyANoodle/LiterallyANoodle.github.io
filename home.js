41030c905e19292f1af44e2e22b1e099d2ffc5b3
Matthew Mahan <themaskedkid321@gmail.com>
2021-11-02 16:29:23 -0500

    making progress on button animation

--- home.js
+++ home.js
@@ -43,23 +43,58 @@ class DefaultButton extends Clickable {
 // create the entrance button and make it look nice
 let frontButton = new DefaultButton();
 frontButton.text = "Clicky clicky.";
+// hacky shit and also not hacky anim shit
+let frontButtonPressed = false;
+let frontPressStopped = true;
+let frontRGB = [0, 0, 0];
 // animation for press and release
 frontButton.onPress = function() {
-	this.color = "#073871";
+	// this.color = "#073871";
 	this.strokeWeight += 1;
-	while (this.text != "") {
-		frontOnPressAnimate();
-	}
+	frontButtonPressed = true;
+}
+frontButton.onRelease = function() {
+	frontButtonPressed = false; 
 }
 // function to animate onPress
 function frontOnPressAnimate() {
-	setTimeout(200);
+	// setTimeout(200);
+	// reduce text
 	frontButton.text = frontButton.text.slice(0, -1);
+
+	// change color 
+	if (frontRGB[0] < 7) {
+		frontRGB[0] += 2;
+	} else {
+		frontRGB[0] = 7;
+	}
+	if (frontRGB[1] < 38) {
+		frontRGB[1] += 2;
+	} else {
+		frontRGB[1] = 38;
+	}
+	if (frontRGB[2] < 71) {
+		frontRGB[2] += 2;
+	} else {
+		frontRGB[2] = 71;
+		frontPressStopped = true; 
+	}
+
+	console.log(frontRGB);
+	frontButton.color = frontRGB;
 }
 
 
 function draw() {
 
+	background(0);
+
+	if ((frontButtonPressed) && !frontPressStopped) {
+		setTimeout(200);
+		frontOnPressAnimate();
+	} else {
+		frontButtonPressed = false; 
+	}
 
 	frontButton.locate((windowWidth / 2) - (frontButton.width / 2), (windowHeight / 2) - (frontButton.height / 2));
 	frontButton.draw();
